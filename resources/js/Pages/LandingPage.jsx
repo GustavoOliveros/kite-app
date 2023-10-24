d27ea8bb7826747b195a9ef6c3034cdbb5dda24c
gustavooliveros <gusa05+1@gmail.com>
2023-10-24 18:13:41 -0300

    Comienzo de 75%

--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -11,7 +11,7 @@ export default function LandingPage({  }) {
             <div className="bg-zinc-900 min-h-screen flex flex-col items-center justify-center">
                 <ApplicationLogo className="w-28" />
                 <h1 className="text-2xl text-white font-bold my-5">
-                    Kite App <span className="text-sm">v0.50</span>
+                    Kite App <span className="text-sm">v0.75</span>
                 </h1>
                 <div className="grid grid-cols-2 gap-4">
                     <LinkButton
