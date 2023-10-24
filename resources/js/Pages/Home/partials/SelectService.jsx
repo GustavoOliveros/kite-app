80035842488dd16c51f4af38a1a7f133ee3425dd
gustavooliveros <gusa05+1@gmail.com>
2023-10-24 18:06:23 -0300

    Cambios mínimos visuales

--- resources/js/Pages/Home/partials/SelectService.jsx
+++ resources/js/Pages/Home/partials/SelectService.jsx
@@ -1,14 +1,16 @@
-import { Link } from "@inertiajs/react"
 export default function SelectService({ service }) {
     return (
         <a
-            className='flex justify-between p-10 mx-8 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'
+            className="flex justify-between p-10 mx-8 bg-gray-900 border-2 border-transparent rounded-md font-semibold text-xs text-white uppercase hover:border-white hover:border-2 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out delay-150 hover:scale-110 duration-300"
             disabled={false}
             href={service.homepage}
-            target='_blank'
+            target="_blank"
         >
-            <img src={service.logo_path} className="w-20 h-20" alt={service.name} />
+            <img
+                src={service.logo_path}
+                className="w-20 h-20"
+                alt={service.name}
+            />
         </a>
-    )
-    
+    );
 }
--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -1,21 +1,31 @@
-import { Head } from '@inertiajs/react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import LinkButton from '@/Components/LinkButton';
+import { Head } from "@inertiajs/react";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import LinkButton from "@/Components/LinkButton";
 
-export default function LandingPage({ }) {
+export default function LandingPage({  }) {
+
+
     return (
         <>
             <Head title="Todo en un solo lugar" />
             <div className="bg-zinc-900 min-h-screen flex flex-col items-center justify-center">
                 <ApplicationLogo className="w-28" />
-                <h1 className="text-2xl text-white font-bold my-5">Kite App <span className="text-sm">v0.50</span></h1>
+                <h1 className="text-2xl text-white font-bold my-5">
+                    Kite App <span className="text-sm">v0.50</span>
+                </h1>
                 <div className="grid grid-cols-2 gap-4">
-                    <LinkButton disabled={false} href={route('login')} title="Iniciar sesión"  />
-                    <LinkButton disabled={false} href={route('register')} title="Crear cuenta" />
+                    <LinkButton
+                        disabled={false}
+                        href={route("login")}
+                        title="Iniciar sesión"
+                    />
+                    <LinkButton
+                        disabled={false}
+                        href={route("register")}
+                        title="Crear cuenta"
+                    />
                 </div>
-
             </div>
-            
         </>
     );
 }
